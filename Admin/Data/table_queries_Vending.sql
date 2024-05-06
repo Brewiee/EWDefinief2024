c844d6b4bf9ade00b81a1ddf3b28ed40a42a7b96
Joris Daniels <daniels.joris9@gmail.com>
2024-05-06 20:27:44 +0200

    Working deployment

--- Admin/Data/table_queries_Vending.sql
+++ Admin/Data/table_queries_Vending.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS user (
-    vd_user_id INT AUTO_INCREMENT PRIMARY KEY,
-    vd_user_name VARCHAR(255),
-    vd_user_password VARCHAR(255),
-    vd_user_function VARCHAR(255),
-    vd_user_firstname VARCHAR(255),
-    vd_user_lastname VARCHAR(255)
+CREATE TABLE IF NOT EXISTS vending_machine (
+    vd_vending_machine_ID INT AUTO_INCREMENT PRIMARY KEY,
+    vd_vending_machine_location VARCHAR(255),
+    vd_vending_machine_address VARCHAR(255),
+    vd_vending_machine_postal_code VARCHAR(255),
+    vd_vending_machine_city VARCHAR(255),
+    vd_vending_machine_country VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS category (
@@ -22,6 +22,11 @@ CREATE TABLE IF NOT EXISTS product (
     FOREIGN KEY (vd_product_category) REFERENCES category(vd_category_id)
 );
 
+CREATE TABLE IF NOT EXISTS payment (
+    vd_payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    vd_payment_method VARCHAR(50)
+);
+
 CREATE TABLE IF NOT EXISTS inventory (
     vd_inventory_id INT AUTO_INCREMENT PRIMARY KEY,
     vd_inventory_vending_machine_id INT,
@@ -30,18 +35,8 @@ CREATE TABLE IF NOT EXISTS inventory (
     vd_inventory_min_stock INT,
     vd_inventory_max_stock INT,
     vd_inventory_refill_stock INT,
-    FOREIGN KEY (vd_inventory_product_id) REFERENCES product(vd_product_id)
-);
-
-CREATE TABLE IF NOT EXISTS vending_machine (
-    vd_vending_machine_ID INT AUTO_INCREMENT PRIMARY KEY,
-    vd_vending_machine_location VARCHAR(255),
-    vd_vending_machine_address VARCHAR(255),
-    vd_vending_machine_postal_code VARCHAR(255),
-    vd_vending_machine_city VARCHAR(255),
-    vd_vending_machine_country VARCHAR(255),
-    FOREIGN KEY (vd_vending_machine_id) REFERENCES inventory(vd_inventory_vending_machine_id),
-    FOREIGN KEY (vd_vending_machine_ID) REFERENCES invoice(vd_invoice_vending_machine_id)
+    FOREIGN KEY (vd_inventory_product_id) REFERENCES product(vd_product_id),
+    FOREIGN KEY (vd_inventory_vending_machine_id) REFERENCES vending_machine(vd_vending_machine_ID)
 );
 
 CREATE TABLE IF NOT EXISTS supplier (
@@ -65,10 +60,6 @@ CREATE TABLE IF NOT EXISTS invoice (
     vd_invoice_time TIME,
     vd_invoice_payment_id INT,
     FOREIGN KEY (vd_invoice_product_id) REFERENCES product(vd_product_id),
-    FOREIGN KEY (vd_invoice_payment_id) REFERENCES payment(vd_payment_id)
-);
-
-CREATE TABLE IF NOT EXISTS payment (
-    vd_payment_id INT AUTO_INCREMENT PRIMARY KEY,
-    vd_payment_method VARCHAR(10)
+    FOREIGN KEY (vd_invoice_payment_id) REFERENCES payment(vd_payment_id),
+    FOREIGN KEY (vd_invoice_vending_machine_id) REFERENCES vending_machine(vd_vending_machine_ID)
 );
